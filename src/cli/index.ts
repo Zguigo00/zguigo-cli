a664740c8d7a30433802aa2e4f06493af9fbd884
Zguigo <1853448814@qq.com>
2026-09-10 12:57:04 +0800

    chore: 删除未使用的 render.ts，更新 cli/index.ts 导出

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,4 @@
 export { startRepl } from './repl.js';
 export { handleCommand, commands, getCommandNames } from './commands.js';
-export { renderStream } from './render.js';
+export { BoxRenderer } from './box.js';
+export { handlePlanCommand, handleRunCommand } from './plan-runner.js';
